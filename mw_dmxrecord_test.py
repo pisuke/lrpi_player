5fb65f7ea7e19df7a771042dc3a6e614ff12f4ad
Francesco Anselmo <francesco.anselmo@gmail.com>
2018-12-05 01:25:17 +0000

    Added Philips Hue player from srt file

--- mw_dmxrecord_test.py
+++ mw_dmxrecord_test.py
@@ -1,125 +1,45 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 HOST = "127.0.0.1"
 PORT = 4223
 
-debug = True
+debug = False
 verbose = True
 reading_interval = 1.0
 
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_dmx import BrickletDMX
 
-from time import sleep
+from time import sleep, perf_counter
 from os.path import join, splitext
 from os import listdir
 import math, time, datetime
+from numpy import array, zeros, array_equal
+import pysrt
+import signal
+import sys
+from pysrt import SubRipFile
+from pysrt import SubRipItem
+from pysrt import SubRipTime
 
-tfIDs = [
-]
+from tf_device_ids import deviceIdentifiersList
+
+srtFilename = "output.srt"
+srtFile = SubRipFile()
+
+tfIDs = []
 
 tfConnect = True
 
-if tfConnect:
-    tfIDs = []
+prevFrame = zeros(512)
+prevTime = 0
+subs = []
 
-deviceIdentifiersList = [
-[11, "DC Brick",""],
-[13, "Master Brick",""],
-[14, "Servo Brick",""],
-[15, "Stepper Brick",""],
-[16, "IMU Brick","sensor",["get_all_data"],[""]],
-[17, "RED Brick",""],
-[18, "IMU Brick 2.0","sensor",["get_all_data"],[""]],
-[19, "Silent Stepper Brick",""],
-[21, "Ambient Light Bricklet","sensor",["get_illuminance"],["lux"]],
-[23, "Current12 Bricklet","sensor"],
-[24, "Current25 Bricklet","sensor"],
-[25, "Distance IR Bricklet","sensor"],
-[26, "Dual Relay Bricklet","actuator"],
-[27, "Humidity Bricklet","sensor",["get_humidity"],["%RH"]],
-[28, "IO-16 Bricklet","sensor"],
-[29, "IO-4 Bricklet","sensor"],
-[210, "Joystick Bricklet","sensor"],
-[211, "LCD 16x2 Bricklet","actuator"],
-[212, "LCD 20x4 Bricklet","actuator"],
-[213, "Linear Poti Bricklet","sensor"],
-[214, "Piezo Buzzer Bricklet","actuator"],
-[215, "Rotary Poti Bricklet","sensor"],
-[216, "Temperature Bricklet","sensor",["get_temperature"],["ºC"]],
-[217, "Temperature IR Bricklet","sensor"],
-[218, "Voltage Bricklet","sensor"],
-[219, "Analog In Bricklet","sensor"],
-[220, "Analog Out Bricklet","actuator"],
-[221, "Barometer Bricklet","sensor"],
-[222, "GPS Bricklet","sensor"],
-[223, "Industrial Digital In 4 Bricklet","sensor"],
-[224, "Industrial Digital Out 4 Bricklet","actuator"],
-[225, "Industrial Quad Relay Bricklet","actuator"],
-[226, "PTC Bricklet","sensor"],
-[227, "Voltage/Current Bricklet","sensor"],
-[228, "Industrial Dual 0-20mA Bricklet",""],
-[229, "Distance US Bricklet","sensor"],
-[230, "Dual Button Bricklet","sensor"],
-[231, "LED Strip Bricklet","actuator"],
-[232, "Moisture Bricklet","sensor"],
-[233, "Motion Detector Bricklet","sensor"],
-[234, "Multi Touch Bricklet","sensor"],
-[235, "Remote Switch Bricklet","sensor"],
-[236, "Rotary Encoder Bricklet","sensor"],
-[237, "Segment Display 4x7 Bricklet","actuator"],
-[238, "Sound Intensity Bricklet","sensor", ["get_intensity"],["dB"]],
-[239, "Tilt Bricklet","sensor"],
-[240, "Hall Effect Bricklet","sensor"],
-[241, "Line Bricklet","sensor"],
-[242, "Piezo Speaker Bricklet","actuator"],
-[243, "Color Bricklet","sensor"],
-[244, "Solid State Relay Bricklet","actuator"],
-[245, "Heart Rate Bricklet","sensor"],
-[246, "NFC/RFID Bricklet","sensor"],
-[249, "Industrial Dual Analog In Bricklet","sensor"],
-[250, "Accelerometer Bricklet","sensor"],
-[251, "Analog In Bricklet 2.0","sensor"],
-[252, "Gas Detector Bricklet","sensor"],
-[253, "Load Cell Bricklet","sensor"],
-[254, "RS232 Bricklet",""],
-[255, "Laser Range Finder Bricklet","sensor"],
-[256, "Analog Out Bricklet 2.0","actuator"],
-[257, "AC Current Bricklet",""],
-[258, "Industrial Analog Out Bricklet",""],
-[259, "Ambient Light Bricklet 2.0","",["get_illuminance"],["lux"]],
-[260, "Dust Detector Bricklet","sensor"],
-[261, "Ozone Bricklet","sensor"],
-[262, "CO2 Bricklet","sensor"],
-[263, "OLED 128x64 Bricklet","actuator"],
-[264, "OLED 64x48 Bricklet","actuator"],
-[265, "UV Light Bricklet","sensor"],
-[266, "Thermocouple Bricklet","sensor"],
-[267, "Motorized Linear Poti Bricklet","sensor"],
-[268, "Real-Time Clock Bricklet",""],
-[269, "Pressure Bricklet","sensor"],
-[270, "CAN Bricklet",""],
-[271, "RGB LED Bricklet","actuator"],
-[272, "RGB LED Matrix Bricklet","actuator"],
-[276, "GPS Bricklet 2.0","sensor"],
-[277, "RS485 Bricklet",""],
-[278, "Thermal Imaging Bricklet",""],
-[282, "RGB LED Button Bricklet","sensor"],
-[283, "Humidity Bricklet 2.0","sensor"],
-[284, "Dual Relay Bricklet 2.0","actuator"],
-[285, "DMX Bricklet","actuator"],
-[286, "NFC Bricklet","sensor"],
-[287, "Moisture Bricklet 2.0","sensor"],
-[288, "Outdoor Weather Bricklet","sensor"],
-[289, "Remote Switch Bricklet 2.0","actuator"],
-[291, "Temperature IR Bricklet 2.0","sensor"],
-[292, "Motion Detector Bricklet 2.0","sensor"],
-[294, "Rotary Encoder Bricklet 2.0","sensor"],
-[295, "Analog In Bricklet 3.0","sensor"],
-[296, "Solid State Relay Bricklet 2.0","actuator"],
-[21111, "Stream Test Bricklet",""],
-]
+ipcon = IPConnection()
+
+# if tfConnect:
+#     tfIDs = []
 
 deviceIDs = [i[0] for i in deviceIdentifiersList]
 
@@ -143,12 +63,39 @@ def cb_enumerate(uid, connected_uid, position, hardware_version, firmware_versio
 
 
 def dmxread_callback(frame, frame_no):
-    print(frame, frame_no)
+    global prevFrame, prevTime, subs, srtFile
+    # if prevFrame. == 0:
+    #     prevFrame = array(frame)
+    frameArray = array(frame)
+    if not array_equal(prevFrame,frameArray):
+        if frame != None:
+            item = SubRipItem(1, text="DMX1"+str(frame))
+            item.shift(seconds=prevTime)
+            item.end.shift(seconds=perf_counter()-prevTime)
+            print(item)
+            subs.append(item)
+            srtFile.append(item)
+            prevTime = perf_counter()
+    prevFrame = array(frame)
+    # if prevframe-frame:
+    #     print(frame, frame_no)
+    # prev
     # print("callback called")
 
+def signal_handler(sig, frame):
+    global subs, tfConnect, ipcon, srtFile
+    print(subs, len(subs))
+    encoding="utf_8"
+
+    srtFile.save(srtFilename, encoding=encoding)
+
+    if tfConnect:
+        ipcon.disconnect()
+    sys.exit(0)
+
 if __name__ == "__main__":
-    # Create connection and connect to brickd
-    ipcon = IPConnection()
+    # global ipcon
+
     ipcon.connect(HOST, PORT)
 
     # Register Enumerate Callback
@@ -177,12 +124,10 @@ if __name__ == "__main__":
                         dmx = BrickletDMX(tf[0], ipcon)
                         dmx.set_dmx_mode(BrickletDMX.DMX_MODE_SLAVE)
                         dmx.register_callback(BrickletDMX.CALLBACK_FRAME, dmxread_callback)
-                        dmx.set_frame_callback_config(True, True, True, True)
+                        dmx.set_frame_callback_config(False, False, True, False)
+                        signal.signal(signal.SIGINT, signal_handler)
 
                     dmxcount += 1
 
     while True:
         pass
-
-    if tfConnect:
-        ipcon.disconnect()
