8c266cd28179bf30db2e236c1f1f0568196e296a
InBrewJ <jason@urawizard.com>
2023-06-06 08:57:46 +0100

    Adds note about build script no longer working - but we're close

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # lushroom-player Dockerfile
 
+# NOTE build isn't working at the moment because <something something Debian package lists>
+# see here: https://stackoverflow.com/a/76094521/8249410
+
 if [ $# -eq 0 ]; then
     echo "Please specific build tag as your first argument!"
     echo "e.g. ./build.sh latest"
